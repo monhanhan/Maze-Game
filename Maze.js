0cb4e371a54cfab52a6e603c0bc5560c00375514
Ryan Munin <ryan.munin@gmail.com>
2022-11-16 19:17:19 -0800

    Debugging needed for wall location

--- Maze.js
+++ Maze.js
@@ -1,3 +1,11 @@
+/**
+ * This class procedurally generates a maze by recursively drawing walls.
+ *
+ * The level attribute represents a 2d array representation of the maze and where
+ * the walls are located.
+ *
+ * The visited attribute tracks if a given maze position has been visited by a minotaur.
+ */
 class Maze {
   /*
    * This class has the following attributes:
@@ -16,7 +24,7 @@ class Maze {
 
   constructor() {
     //Set all my constants
-    this.ICON_WALL = "#";
+    this.ICON_WALL = "*";
     this.ICON_BLANK = " ";
     this.ICON_KEY = "K";
     this.ICON_EXIT = "E";
@@ -48,7 +56,11 @@ class Maze {
       this.visited[i] = new Array(this.LEVEL_WIDTH);
     }
 
+    // Add exterior walls to the maze
     this.createBlankLevel();
+
+    // recursively draw the inside of the maze.
+    this.makeMazeRecursive(1, 1, this.LEVEL_WIDTH - 2, this.LEVEL_HEIGHT - 2);
   }
 
   /**
@@ -89,10 +101,79 @@ class Maze {
       this.visited[y][this.LEVEL_WIDTH - 1] = true;
     }
   }
+
+  makeMazeRecursive(startX, startY, endX, endY) {
+    //If the section into which we are adding walls is smaller than 3 units in any direction, do not add walls. Return instead. This is the base case.
+    if (endX - startX <= 3 || endY - startY <= 3) {
+      return;
+    } else {
+      // TODO: some of the walls are doubling up. find out why and fix it.
+      // TODO: Find a way to prevent inaccessable cells.
+
+      // randomly generate the wall locations
+      var xWallLoc = this.randBetween(startY + 2, endY - 2);
+      var yWallLoc = this.randBetween(startX + 2, endX - 2);
+
+      // Modify maze so that the walls are actually drawn
+      for (let x = startX; x <= endX; x++) {
+        this.level[xWallLoc][x] = this.ICON_WALL;
+        this.visited[xWallLoc][x] = true;
+      }
+
+      for (let y = startY; y <= endY; y++) {
+        this.level[y][yWallLoc] = this.ICON_WALL;
+        this.visited[y][yWallLoc] = true;
+      }
+
+      // Generates where the openings in the walls should go
+      var xOpenLoc = this.randBetween(startX + 1, endX - 1);
+
+      // Check to ensure the openings don't go in cross sections of the
+      // walls.
+      while (xOpenLoc == yWallLoc) {
+        xOpenLoc = this.randBetween(startX + 1, endX - 1);
+      }
+
+      // Put the empty spot in the horizontal wall
+      this.level[xWallLoc][xOpenLoc] = this.ICON_BLANK;
+      this.visited[xWallLoc][xOpenLoc] = this.ICON_BLANK;
+
+      // Generate the opening in the vertical wall
+      var yOpenLoc = this.randBetween(startY + 1, endY - 1);
+
+      // If the opening would fall in a cross section generate a new opening instead.
+      while (yOpenLoc == xWallLoc) {
+        yOpenLoc = this.randBetween(startY + 1, endY - 1);
+      }
+
+      // Put the opening in the vertical wall
+      this.level[yOpenLoc][yWallLoc] = this.ICON_BLANK;
+      this.visited[yOpenLoc][yWallLoc] = false;
+
+      // recurse using the dimensions of our new mini-maze to make a smaller set of walls inside the ones we just made.
+
+      // Upper-left corner
+      this.makeMazeRecursive(startX, startY, yWallLoc, xWallLoc);
+
+      // upper-right corner
+      this.makeMazeRecursive(yWallLoc, startY, endX, xWallLoc);
+
+      // lower-left corner
+      this.makeMazeRecursive(startX, xWallLoc, yWallLoc, endY);
+
+      // lower-right corner
+      this.makeMazeRecursive(yWallLoc, xWallLoc, endX, endY);
+    }
+  }
+
+  randBetween(start, end) {
+    var range = end - start;
+    return Math.floor(Math.random() * range) + 1 + start;
+  }
 }
 
 // Below is debug code that will print the maze to the console. Consider repurposing it later to represent the maze.
-/*
+
 const myMaze = new Maze();
 
 var mazePrint = "";
@@ -105,4 +186,3 @@ for (let i = 0; i < myMaze.level.length; i++) {
 }
 
 console.log(mazePrint);
-*/
